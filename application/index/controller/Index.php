e38a028f56480d774298d260c40544f643fadd5c
hehuping <595549109@qq.com>
2017-05-29 21:43:32 +0800

    主流程

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -2,9 +2,13 @@
 namespace app\index\controller;
 
 use TencentYoutuyun\Conf;
+use TencentYoutuyun\YouTu;
+use think\Db;
 
 class Index
 {
+
+    protected $base_url = 'https://www.yiyazhe.com/uploads/';
 
     public function __construct()
     {
@@ -21,8 +25,12 @@ class Index
         return 1;
     }
 
-    public function addFace()
+
+    public function uploadFace()
     {
+         $group_id = 'test_group_id';
+        $person_id = 'test_person_id';
+        header('Content-type: application/json');
         $uploads_dir = ROOT_PATH . '/public/uploads';
         if (!is_dir($uploads_dir)) {
             mkdir($uploads_dir);
@@ -31,8 +39,59 @@ class Index
         if ($_FILES ["file"]["error"] == 0) {
             $tmp_name = $_FILES ["file"]["tmp_name"];
             $name = time() . $_FILES ["file"]["name"];
-            move_uploaded_file($tmp_name, "$uploads_dir/$name");
+            if(move_uploaded_file($tmp_name, "$uploads_dir/$name")){
+                //搜索topface
+                $res = YouTu::faceidentifyurl($this->base_url.$name, $group_id);
+                //新增优图个体
+               $re = YouTu::newperson($this->base_url.$name, $person_id, [$group_id],$person_id, $this->base_url.$name);
+                //插入数据库
+                Db::table('face')->data(
+                    [
+                        'face_id' => $re['face_id'],
+                        'user_id' => $person_id,
+                        'nickName'=>$person_id,
+                        'img_url' => $this->base_url.$name,
+                        'we_group_id' => $group_id,
+                        'youtu_group_id' => $group_id,
+                        'appid' => '',
+                    ]
+                )->insert();
+
+                return json($res);
+
+            }else{
+                return json(['error'=> -1]);
+            }
+
+        }else{
+            return json(['error'=> -2]);
         }
-        return json(['filename' => $name, 'size'=> $_FILES ["file"]['size']]);
+
+    }
+
+    protected function addToPerson($filename, $groupid){
+        //新增个体
+       return  YouTu::newperson($this->base_url.$filename, 'test_id', ['test_group'],'test_name', '');
+    }
+
+    protected function Top5Face($path){
+        YouTu::faceidentify($this->base_url.$path);
+    }
+
+    //获取组
+    public function getGroup(){
+        $group_id = YouTu::getgroupids();
+        return json($group_id);
+    }
+
+    //获取人列表
+    public function getPerson($id){
+        $id = $id ? $id : $_GET['id'];
+        return json(YouTu::getpersonids($id));
+    }
+
+    public function getFace($id){
+        $id = $id ? $id : $_GET['id'];
+        return json(YouTu::getfaceids($id));
     }
 }
