12fdcad229659af816ddb9162d9d4f06b9580ad8
hehuping <595549109@qq.com>
2017-05-29 11:27:29 +0800

    init

---
+++ application/index/controller/Index.php
@@ -0,0 +1,27 @@
+<?php
+namespace app\index\controller;
+
+use TencentYoutuyun\Conf;
+
+class Index
+{
+
+    public function __construct()
+    {
+        $appid='10008634';
+        $secretId='AKIDBE9sQsEbPxxyvgIsU32xmZCtVuDkn33v';
+        $secretKey='542QiuwmU6WkYIsHanroaGyX2SjokVEv';
+        $userid='595549109';
+        $end_point = 'http://api.youtu.qq.com/';
+        Conf::setAppInfo($appid,$secretId,$secretKey,$userid,$end_point);
+    }
+
+    public function index()
+    {
+       return 1;
+    }
+
+    public function addFace(){
+        return json($_SERVER);
+    }
+}
