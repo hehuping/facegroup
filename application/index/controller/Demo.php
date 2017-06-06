2f2828e2712d638493f7303bf646cd31cd7e73f3
hehuping <595549109@qq.com>
2017-06-06 23:19:23 +0800

    fds

--- application/index/controller/Demo.php
+++ application/index/controller/Demo.php
@@ -29,11 +29,11 @@ class Demo extends \think\Controller
 
         $ticket = (array)json_decode($json);
         $noncestr=str_shuffle('Wm3WZYTPz0wzccnW');
-        $jsapi_ticket=$ticket['jsapi_ticket'];
+        $jsapi_ticket=$ticket['ticket'];
         $timestamp=time();
         $w_url='https://www.yiyazhe.com/index/demo/getSgin';
 
-        $jsapi_ticket=$ticket.'&noncestr='.$noncestr.'&timestamp='.$timestamp.'&url='.$w_url;
+        $jsapi_ticket=$jsapi_ticket.'&noncestr='.$noncestr.'&timestamp='.$timestamp.'&url='.$w_url;
         $str = sha1($jsapi_ticket);
 
 
